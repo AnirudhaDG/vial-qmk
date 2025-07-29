458dbe61e2949f3d48a4129b230d2a374c803b8e
anirudha-dg <anirudhaduttagupta2004@gmail.com>
2025-07-30 04:29:13 +0530

    CmtKey added

---
+++ keyboards/cmtkey/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// #define LED_NUM_LOCK_PIN A8
+
+#define AZOTEQ_IQS5XX_TPS65
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN B7
+#define I2C1_SCL_PIN A15
---
+++ keyboards/cmtkey/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+//VIAL keyboard configuration
+#define VIAL_KEYBOARD_UID {0x51, 0x46, 0xAC, 0x0E, 0xB0, 0x38, 0xD8, 0x0B}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
