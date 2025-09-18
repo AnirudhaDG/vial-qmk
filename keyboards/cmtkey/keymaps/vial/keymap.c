88dbcf627d7b3fd1f449bb87a64d076eff6fce49
anirudha-dg <anirudhaduttagupta2004@gmail.com>
2025-09-19 02:53:21 +0530

    4x4 tester redudancy

--- keyboards/cmtkey/keymaps/vial/keymap.c
+++ keyboards/cmtkey/keymaps/vial/keymap.c
@@ -16,11 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┴┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴───┼───┼───┼───┼───┼───┤
      * │Ctrl│GUI │Alt │                        │Alt│Ctl│ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
-     */ 
-    [0] = LAYOUT_test_4x4(
-        KC_Q, KC_W, KC_E, KC_R,
-        KC_T, KC_Y, KC_U, KC_I,
-        KC_O, KC_P, KC_A, KC_N,
-        KC_S, KC_D, KC_LCTL, KC_SPC
-    )
+     */
+[0] = LAYOUT_final(
+    KC_LEFT,  KC_ENT,    KC_EQL,   KC_ESC,    // top control row: <, ○, =, ✶
+
+    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
+    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_ENT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,      KC_V,   KC_B,   KC_N,   KC_M,   KC_UP,  KC_BSPC,
+    KC_FN0,   KC_LCTL,  KC_LALT,  KC_DOT,    KC_SPC,          KC_TAB, KC_LEFT, KC_DOWN, KC_RGHT
+),
+
 };
